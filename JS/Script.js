8f69cb6354d59fdaaf3b99f928e29052eb8f4c56
ashley <25075152@edgehill.ac.uk>
2024-02-06 23:32:29 +0000

    html and base css structure

---
+++ JS/Script.js
@@ -0,0 +1,5 @@
+const housePriceInput = document.getElementById('house-price');
+const depositAmountInput = document.getElementById('deposit-amount');
+const interestRateInput = document.getElementById('interest-rate');
+const mortgageTermInput = document.getElementById('mortgage-term');
+
